2f41f35053710a6ff9cf02d0f2486feddc86c660
Parnaz-tls <parnaz.tls@gmail.com>
2024-02-21 23:43:57 +0330

    'CCC'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './+home/home.component';
+import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent,ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './+home/home.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'/home', component: HomeComponent},
+    {path:'/products', component: ProductsComponent},
+    {path:'',redirectTo:'/home',pathMatch:'full'},
+    {path:'**', redirectTo:'/home'}
+
+];
