91b4743960faff4fa46bda62ecca462576ff85a0
Parnaz-tls <parnaz.tls@gmail.com>
2024-02-21 23:23:28 +0330

    'fff'

--- src/app/+home/home.component.ts
+++ src/app/+home/home.component.ts
@@ -4,9 +4,9 @@ import { Component } from "@angular/core";
 @Component({
     selector:'tls-server',
     standalone:true,
-    template:`<p>salam</p>`,
-    imports:[]
-
+    imports:[],
+    templateUrl:'./home.component.html',
+    styleUrl:'./home.component.scss',
 
 })
 export class HomeComponent{
