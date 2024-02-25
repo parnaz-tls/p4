519bfefb84f71ee3e17c8c6185cff0a51ee04334
Parnaz-tls <parnaz.tls@gmail.com>
2024-02-26 01:12:12 +0330

    'hhh'

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
-
+  @Input() title:string='ProductName'
+  @Input() price:number=0
+  // @Input() img='ProductImage'
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,5 +11,13 @@ import { ProductComponent } from './product/product.component';
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
-
+  products=[
+    {title:"chocolate donut", price:20, img:"../../assets/donuts/pic1.jpg"},
+    {title:"nesscafe donut", price:40 , img:"../../assets/donuts/pic2.jpg"},
+    {title:"strawberry donut",price:30, img:"../../assets/donuts/pic3.jpg"},
+    {title:"nuts donut" ,price:60, img:"../../assets/donuts/pic4.jpg"}, 
+    {title:"blueberry donut" ,price:40, img:"../../assets/donuts/pic5.jpg"},
+    {title:"caramell donut" ,price:20, img:"../../assets/donuts/pic6.jpg"},
+    {title:"cupcake" ,price:50, img:"../../assets/donuts/pic7.jpg"},
+  ]
 }
